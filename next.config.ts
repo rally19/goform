ec96a05cb0bddf9e8725328c70e3578d87510b0c
Leonel R.S. <masterrally1808@gmail.com>
2026-04-12 20:13:59 +0700

    fix next config ts

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  serverActions: {
-    bodySizeLimit: '5mb',
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "5mb",
+    },
   },
 };
 
